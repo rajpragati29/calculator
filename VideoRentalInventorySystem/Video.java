f5401b647feab44840f8ca8ece3fa84f86fcea43
rajpragati29 <65039929+rajpragati29@users.noreply.github.com>
2020-05-09 00:17:33 +0530

    Add files via upload

---
+++ VideoRentalInventorySystem/Video.java
@@ -0,0 +1,39 @@
+package VideoRentalInventorySystem;
+
+public class Video {
+	
+	String videoName;
+	boolean checkout;
+	int rating;
+	
+	Video(String name)
+	{
+		videoName=name;
+	}
+	
+	String getName()
+	{
+		return videoName;
+	}
+	void doCheckout()
+	{
+		checkout= true;
+	}
+	void doReturn()
+	{
+		checkout= false;
+	}
+	void receiveRating(int rating)
+	{
+		this.rating=rating;
+	}
+	int getRating()
+	{
+		return rating;
+	}
+	boolean getCheckout()
+	{
+		return checkout;
+	}
+
+}
